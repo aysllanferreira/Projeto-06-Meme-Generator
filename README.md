b2b47ee54bb23446ae38b6ecc9dbf046f94c1ea4
aysllanferreira <aysllan.ferreiraks@gmail.com>
2022-10-17 06:27:19 -0300

    meme generator made

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
 # Projeto Meme Generator
 
+[Link do Projeto](https://aysllanferreira.github.io/Projeto-06-Meme-Generator/)
 ![Project Meme Generator](./memeGen.png)
--- script.js
+++ script.js
@@ -4,6 +4,43 @@ const getBtnFire = document.querySelector('#fire');
 const getBtnWater = document.querySelector('#water');
 const getBtnEarth = document.querySelector('#earth');
 const getImgPreview = document.querySelector('#meme-image');
+const getDarkBtn = document.querySelector('#darkMode');
+const body = document.querySelector('body');
+
+const darkMode = () => {
+  const get = getComputedStyle(body).backgroundColor;
+  if(get === 'rgb(245, 245, 245)'){
+    body.style.backgroundColor = 'rgb(0,0,0)';
+    body.style.color = 'white';
+    getDarkBtn.innerHTML = '<i class="fa-solid fa-sun"></i>';
+    getDarkBtn.style.backgroundColor = 'black';
+    getDarkBtn.style.color = 'white';
+    localStorage.setItem('darkMode', 'true');
+  }else{
+    body.style.backgroundColor = 'rgb(245, 245, 245)';
+    body.style.color = 'black';
+    getDarkBtn.innerHTML = '<i class="fa-solid fa-moon"></i>';
+    getDarkBtn.style.backgroundColor = 'white';
+    getDarkBtn.style.color = 'black';
+    localStorage.setItem('darkMode', 'false');
+  }
+};
+
+const verifyDarkMode = () => {
+  if (localStorage.getItem('darkMode') === 'true') {
+    body.style.backgroundColor = 'rgb(0,0,0)';
+    body.style.color = 'white';
+    getDarkBtn.innerHTML = '<i class="fa-solid fa-sun"></i>';
+    getDarkBtn.style.backgroundColor = 'black';
+    getDarkBtn.style.color = 'white';
+  }else{
+    body.style.backgroundColor = 'rgb(245, 245, 245)';
+    body.style.color = 'black';
+    getDarkBtn.style.backgroundColor = 'white';
+    getDarkBtn.style.color = 'black';
+    getDarkBtn.innerHTML = '<i class="fa-solid fa-moon"></i>';
+  }
+};
 
 const textPreview = () => {
   const getInput = document.querySelector('#text-input');
@@ -63,9 +100,11 @@ const createMiniMeme = () => {
 };
 
 window.onload = () => {
+  getDarkBtn.addEventListener('click', darkMode);
   getImgInput.addEventListener('input', uploadImage);
   textPreview();
   setBtnsColours();
   putButtonsToWork();
   createMiniMeme();
+  verifyDarkMode();
 };
